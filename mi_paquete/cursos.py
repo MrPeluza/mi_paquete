9474167f5a0fd61f051518019f030b79e740a37d
ManuelRivera <stefariver456@gmail.com>
2026-01-29 14:07:05 -0500

    Se creo el scrip de cursos

--- mi_paquete/cursos.py
+++ mi_paquete/cursos.py
@@ -1,1 +1,17 @@
+class curso:
+    def __init__(self, nombre, duracion):
+        self.nombre = nombre
+        self.duracion = duracion
 
+    def __repr__(self):
+        return f"{self.nombre} [{self.duracion} horas]"
+
+cursos = [
+    curso("Python", 25),
+    curso("Javascript", 30),
+    curso("HTML", 20)
+]
+
+def listar_cursos():
+    for curso in cursos:
+        print(curso)
